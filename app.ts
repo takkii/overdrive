c3b35a9a03c28de48cf2f69e6c62c69ced2577c0
T・K <karuma.reason@gmail.com>
2025-11-12 08:39:25 +0900

    Update.

--- app.ts
+++ app.ts
@@ -7,6 +7,7 @@ class Env {
     path: any;
 
     constructor() {
+        // https://expressjs.com/ja/5x/api.html
         this.express = require("express");
         this.favicon = require('serve-favicon')
         this.path = require('path')
@@ -16,6 +17,7 @@ class Env {
         this.server = this.app.listen(this.port, function () {
             console.log('listening on port: ' + mask);
         });
+        // https://expressjs.com/ja/starter/static-files.html
         this.app.use(this.favicon(this.path.join(__dirname, 'public', 'favicon.ico')))
         this.app.use(this.express.static('public'))
         this.app.set('view engine', 'ejs');

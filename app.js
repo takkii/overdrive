d4cbaf7a94e130122837385d554cc55fe0a3b8ef
T・K <karuma.reason@gmail.com>
2025-11-11 14:00:45 +0900

    Update.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var Env = /** @class */ (function () {
     function Env() {
         this.express = require("express");
+        this.favicon = require('serve-favicon');
+        this.path = require('path');
         this.app = this.express();
         var mask = process.argv[2];
         this.port = process.env.PORT || mask;
         this.server = this.app.listen(this.port, function () {
             console.log('listening on port: ' + mask);
         });
+        this.app.use(this.favicon(this.path.join(__dirname, 'public', 'favicon.ico')));
         this.app.set('view engine', 'ejs');
     }
     Env.prototype.run = function () {

60b09a7b9ec8f24d96329c1be217debde7012d0d
T・K <karuma.reason@gmail.com>
2025-11-15 15:18:47 +0900

    Update.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var node_fs_1 = require("node:fs");
 var Env = /** @class */ (function () {
     function Env() {
         // https://expressjs.com/ja/5x/api.html
@@ -17,7 +20,19 @@ var Env = /** @class */ (function () {
     }
     Env.prototype.run = function () {
         this.app.get("/", function (req, res) {
-            res.render("index", { title: 'Overdrive' });
+            var json_data = './json/data.json';
+            var data = JSON.parse((0, node_fs_1.readFileSync)("".concat(json_data), 'utf8'));
+            res.locals.title = data.title;
+            res.locals.data = data.dtcl;
+            res.locals.data_full = data.dtcl_full;
+            res.locals.neovim = data.neovim;
+            res.locals.jetbrains = data.jetbrain;
+            res.locals.reason = data.reason;
+            res.locals.settings = data.settings;
+            res.locals.plugins = data.plugins;
+            res.locals.ides = data.ides;
+            res.locals.copy = data.copyright;
+            res.render("index");
             console.log(req.method + ": " + req.protocol);
         });
     };

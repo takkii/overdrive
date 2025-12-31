699911934a721cb23e2f992d38f24db2a74a8a51
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-01-01 08:33:31 +0900

    Update.

--- public/js/copyright.js
+++ public/js/copyright.js
@@ -1,9 +1,5 @@
 OneDay = new Date();
 OneYear = this.OneDay.getFullYear();
 
-if (OneYear == 2025) {
-    var cpr_2025 = document.getElementById("cpr");
-    cpr_2025.innerHTML = "Copyright &copy; 2025 Takayuki Kamiyama. ";
-} else {
-    cpr_2025.innerHTML = "Copyright &copy; 2025-" + OneYear + " Takayuki Kamiyama.";
-}
+var cpr_2025 = document.getElementById("cpr");
+cpr_2025.innerHTML = "Copyright © 2025-" + OneYear + " Takayuki Kamiyama.";
